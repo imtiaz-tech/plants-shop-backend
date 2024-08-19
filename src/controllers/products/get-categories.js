6cff244055e3b438f5b289151cefda50cb60e7b4
Imtiaz Ahmad <imtiazahmadswl93@gmail.com>
2024-08-20 03:15:13 +0500

    add pagination

--- src/controllers/products/get-categories.js
+++ src/controllers/products/get-categories.js
@@ -1,20 +1,25 @@
 import Category from "../../models/category";
 
-
 const getCategories = async (req, res) => {
-    try {
-      const data = await Category.find();
-      return res.status(200).json({
-        data,
-        success: true,
-        message: "Get category Succesfully",
-      });
-    } catch (error) {
-      return res.status(500).json({ success: false, message: error.message });
-    }
-  };
-  
-  export default getCategories;
+  try {
+    let { pageno, perpage } = req.query;
+    pageno = parseInt(pageno) || 1;
+    perpage = parseInt(perpage) || 10;
+    const data = await Category.find()
+      .skip((pageno - 1) * perpage)
+      .limit(perpage);
 
-  
-  
+    const count = await Category.count();
+
+    return res.status(200).json({
+      data,
+      count,
+      success: true,
+      message: "Get category Succesfully",
+    });
+  } catch (error) {
+    return res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+export default getCategories;
