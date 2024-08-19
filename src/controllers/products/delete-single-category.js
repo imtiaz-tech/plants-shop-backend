37ea97feb149f0cbbdff8115c1f3a3482ee94f90
Imtiaz Ahmad <imtiazahmadswl93@gmail.com>
2024-08-19 11:58:15 +0500

    remove consle

--- src/controllers/products/delete-single-category.js
+++ src/controllers/products/delete-single-category.js
@@ -4,14 +4,12 @@ import Category from "../../models/category";
 const deleteSingleCategory = async(req,res)=>{
     try {
         const { id } = req.params;
-        console.log("🚀 ~ deleteSingleCategory ~ id:", id)
         const data = await Category.findByIdAndDelete({ _id: id });
         return res.status(200).json({
           data,
           message: "Deleted Category Succesfully",
         });
       } catch (error) {
-        console.log("🚀 ~ deleteSingleCategory ~ error:", error)
         return res.status(500).json({ message: error.message });
       }
 }
