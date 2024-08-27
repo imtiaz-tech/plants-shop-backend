36af287524be6c8fa775722a29659bcc35712e36
Imtiaz Ahmad <imtiazahmadswl93@gmail.com>
2024-08-27 20:41:14 +0500

    add searching for products

--- src/controllers/unauthrized/get-products.js
+++ src/controllers/unauthrized/get-products.js
@@ -2,13 +2,17 @@ import Product from "../../models/product";
 
 const getProducts = async (req, res) => {
   try {
-    let { pageno, perpage } = req.query;
+    let { pageno, perpage, searchProduct } = req.query;
     pageno = parseInt(pageno) || 1;
     perpage = parseInt(perpage) || 10;
-    const data = await Product.find()
+    let filter = {};
+    if (searchProduct !== "" && searchProduct != "undefined") {
+      filter = { name: { $regex: searchProduct, $options: "i" } };
+    }
+    const data = await Product.find(filter)
       .skip((pageno - 1) * perpage)
       .limit(perpage);
-    const count = await Product.count();
+    const count = await Product.count(filter);
     return res.status(200).json({
       data,
       count,
