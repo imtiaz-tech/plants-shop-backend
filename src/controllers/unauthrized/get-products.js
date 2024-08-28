1b129de0d6e9309cab91849e5ec6ee2b0344d41f
Imtiaz Ahmad <imtiazahmadswl93@gmail.com>
2024-08-28 21:22:00 +0500

    get products by category and get products by price high to low and low to high

--- src/controllers/unauthrized/get-products.js
+++ src/controllers/unauthrized/get-products.js
@@ -1,17 +1,30 @@
 import Product from "../../models/product";
+import mongoose, { Schema } from "mongoose";
 
 const getProducts = async (req, res) => {
   try {
-    let { pageno, perpage, searchProduct } = req.query;
+    let { pageno, perpage, searchProduct, selectCategory, sortBy } = req.query;
     pageno = parseInt(pageno) || 1;
     perpage = parseInt(perpage) || 10;
     let filter = {};
-    if (searchProduct !== "" && searchProduct != "undefined") {
-      filter = { name: { $regex: searchProduct, $options: "i" } };
+    let sort = {};
+    if (searchProduct) {
+      filter.name = { $regex: searchProduct, $options: "i" };
     }
+    if (selectCategory) {
+      filter.categoryId = { $eq: selectCategory };
+    }
+    if (sortBy === "priceHighToLow") {
+      sort.price = -1;
+    } else if (sortBy === "priceLowToHigh") {
+      sort.price = 1;
+    }
+
     const data = await Product.find(filter)
       .skip((pageno - 1) * perpage)
-      .limit(perpage);
+      .limit(perpage)
+      .sort(sort);
+
     const count = await Product.count(filter);
     return res.status(200).json({
       data,
