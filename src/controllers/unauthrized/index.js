7ce1e19e94520e4b6b5e26d2fd11f1c3787b4811
Imtiaz Ahmad <imtiazahmadswl93@gmail.com>
2024-09-03 12:12:00 +0500

    add single product add to cart by id

--- src/controllers/unauthrized/index.js
+++ src/controllers/unauthrized/index.js
@@ -1,4 +1,4 @@
 import getCategories from "./get-categories";
 import getProducts from "./get-products";
-
-export { getCategories, getProducts };
+import getSingleProduct from "./get-single-product";
+export { getCategories, getProducts,getSingleProduct};
--- src/routes/unauthrized.js
+++ src/routes/unauthrized.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { getCategories,getProducts } from "../controllers/unauthrized";
+import { getCategories,getProducts,getSingleProduct } from "../controllers/unauthrized";
 
 
 const router = express.Router();
 
 router.get("/get-categories", getCategories);
 router.get("/get-products", getProducts);
+router.get("/get-single-product/:id", getSingleProduct);
 
 
 export default router;
