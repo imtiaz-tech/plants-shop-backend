e172cdb4ee11d477eefde4d5cd895c3aecb49dbb
Imtiaz Ahmad <imtiazahmadswl93@gmail.com>
2024-09-14 15:20:35 +0500

    add pagination on users list

--- src/controllers/orders/get-users.js
+++ src/controllers/orders/get-users.js
@@ -3,9 +3,15 @@ import Users from "../../models/users";
 
 const getUsers = async (req, res) => {
     try {
-      const data = await Users.find({ userType: "user" });
+      let { currentPage, recordsPerPage } = req.query;
+      currentPage = parseInt(currentPage) || 1;
+    recordsPerPage = parseInt(recordsPerPage) || 10;
+      const data = await Users.find({ userType: "user" }).skip((currentPage - 1) * recordsPerPage)
+      .limit(recordsPerPage);
+      const count = await Users.count({ userType: "user" });
       return res.status(200).json({
         data,
+        count,
         success: true,
         message: "Get users Succesfully",
       });
