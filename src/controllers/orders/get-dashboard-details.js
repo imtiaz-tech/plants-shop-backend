b063363d7b77ed74a77199816d8b14a42d74153e
Imtiaz Ahmad <imtiazahmadswl93@gmail.com>
2024-09-25 17:06:40 +0500

    get data for dashboard details from DB like total order,total sale,average,average item sale,total products add by date,total customer login by date

--- src/controllers/orders/get-dashboard-details.js
+++ src/controllers/orders/get-dashboard-details.js
@@ -1,28 +1,80 @@
 import Order from "../../models/order";
 import moment from "moment";
+import Product from "../../models/product";
+import Users from "../../models/users";
+
 const getDashboardDetails = async (req, res) => {
   try {
-    // const {  } = req.query;
     const { startTime, endTime } = req.body;
     let filter = {};
-    if(startTime && endTime){
-        console.log("🚀 ~ getDashboardDetails ~ endTime:", endTime)
-        console.log("🚀 ~ getDashboardDetails ~ startTime:", startTime)
-        filter.createdAt = {
-            $gte: startTime,
-            $lt: endTime
-        }
+    if (startTime && endTime) {
+      filter.createdAt = {
+        $gte: startTime,
+        $lt: endTime,
+      };
     }
-   
-    const data = await Order.find(filter);
-    console.log("🚀 ~ getDashboardDetails ~ data:", data)
+    const orderCount = await Order.countDocuments({
+      createdAt: {
+        $gte: moment(startTime),
+        $lte: moment(endTime),
+      },
+    });
+    const productCount = await Product.countDocuments({
+        createdAt: {
+          $gte: moment(startTime),
+          $lte: moment(endTime),
+        },
+      });
+      const customerCount = await Users.countDocuments({
+        createdAt: {
+          $gte: moment(startTime),
+          $lte: moment(endTime),
+        },
+      });
+    const orderSalesCount = await Order.aggregate([
+      { $match: { createdAt: { $gte: new Date(startTime), $lt: new Date(endTime) } } },
+      { $unwind: "$cart" },
+      {
+        $project: {
+          totalSaleAmount: { $multiply: ["$cart.unitPrice", "$cart.quantity"] },
+        },
+      },
+      {
+        $group: {
+          _id: null,
+          totalSales: { $sum: "$totalSaleAmount" },
+        },
+      },
+    ]);
+    const orderProductCount = await Order.aggregate([
+        { $match: { createdAt: { $gte: new Date(startTime), $lt: new Date(endTime) } } },
+        { $unwind: "$cart" },
+        {
+          $project: {
+            totalProduct: { $sum: [ "$cart.quantity"] },
+          },
+        },
+        {
+          $group: {
+            _id: null,
+            totalProduct: { $sum: "$totalProduct" },
+          },
+        },
+      ]);
+    const average = orderSalesCount[0]?.totalSales/orderCount;
+    const averageItemSale = orderSalesCount[0]?.totalSales/orderProductCount[0]?.totalProduct;
+    
     return res.status(200).json({
-      data,
+      averageItemSale,
+      customerCount,
+      productCount,
+      average,
+      orderCount,
+      orderSalesCount,
       success: true,
       message: "Get Dashboard details Succesfully",
     });
   } catch (error) {
-    console.log("🚀 ~ getDashboardDetails ~ error:", error)
     return res.status(500).json({ success: false, message: error.message });
   }
 };
